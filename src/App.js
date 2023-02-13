7c388f0e0c4846cdc1c24885fecc738f2ed1d723
Grégory Marini <greg.marini@hotmail.fr>
2023-02-13 14:56:32 +0100

    upload images for portefolio, work on style

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
+import "./App.styles.scss";
 import "./portfolio.styles.scss";
 
 const App = () => {
-    const categories = [
-        { id: 1, title: "React" },
-        { id: 2, title: "Symfony" },
-        { id: 3, title: "node" },
+    const projects = [
+        {
+            id: 1,
+            title: "Forkify",
+            imageURL: "./images/portefolio/pexels-elina-fairytale-3806983.jpg",
+        },
+        {
+            id: 2,
+            title: "Marvel-Quizz",
+            imageURL: "./images/portefolio/marvel.png",
+        },
+        {
+            id: 3,
+            title: "RS garage",
+            imageURL: "./images/portefolio/hero_rs.jpg",
+        },
+        {
+            id: 4,
+            title: "React-Ecom",
+            imageURL: "./images/portefolio/reactEcom.jpg",
+        },
+        {
+            id: 5,
+            title: "Natours",
+            imageURL: "./images/portefolio/natours.jpg",
+        },
+        {
+            id: 6,
+            title: "Today I Learned",
+            imageURL: "./images/portefolio/todayILearned.jpg",
+        },
     ];
     return (
-        <div className="portfolio-container">
-            {categories.map((category) => (
-                <div key={category.id} className="category-container">
-                    {/* <img/> */}
-                    <div className="cat-body-container">
-                        <h2>{category.title}</h2>
-                        <p>my blog</p>
+        <div className="app-container">
+            <div className="portfolio-container">
+                {projects.map(({ title, id, imageURL }) => (
+                    <div key={id} className="project-container">
+                        <div
+                            className="background-image"
+                            style={{ backgroundImage: `${imageURL}` }}
+                        />
+                        <div className="project-body*container">
+                            <h2>{title}</h2>
+                            <p>See Now</p>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 };
