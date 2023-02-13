d4a1b3df661ee47e0de383f450a9a6e22f8a0d3d
Grégory Marini <greg.marini@hotmail.fr>
2023-02-13 11:24:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <div className="portfolio-container">
+                <div className="category-container">
+                    {/* <img/> */}
+                    <div className="cat-body-container">
+                        <h2>symfony</h2>
+                        <p>my blog</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
