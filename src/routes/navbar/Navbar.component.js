eec42b3c4e30313c7fe0ed530f93b59fb477ec9e
Grégory Marini <greg.marini@hotmail.fr>
2023-02-16 18:34:47 +0100

    insert navbar style, need to bee adapted

--- src/routes/navbar/Navbar.component.js
+++ src/routes/navbar/Navbar.component.js
@@ -1,15 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "../home/Home.component";
+import "./navbar.styles.scss";
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </div>
+        <section className="navbar-container">
+            <button className="navbar-container__button">
+                <span className="navbar-container__icon">&nbsp;</span>
+            </button>
+            <div className="navbar">
+                Navbar
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </div>
+        </section>
     );
 };
 
