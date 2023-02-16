d2f6d73e00d22963b81359b5ec5cb4543262bd4d
Grégory Marini <greg.marini@hotmail.fr>
2023-02-16 21:38:55 +0100

    work on style

--- src/components/header/Hero.component.js
+++ src/components/header/Hero.component.js
@@ -3,30 +3,22 @@ import logoDarkGreen from "../../images/logo_original_vert_fonce.png";
 
 export const Hero = () => {
     return (
-        <header
-            id="header"
-            class="header"
-            //         style={{
-            //             backgroundImage: `linear-gradient(
-            //         to right bottom,
-            //         rgba(113,162,35, 0.2),
-            //         rgba(195,237,108, 0.5)
-            //     ),
-            //     url("/images/hero_resize.jpg")
-            // `,
-            //         }}
-        >
-            <div class="header__container">
-                <h1 class="header__myname heading-1">Grégory Marini</h1>
-                <div class="header__logo">
+        <header className="header">
+            <div className="header__container">
+                <h1 className="header__myname heading-1">Grégory Marini</h1>
+                <div className="header__logo">
                     <a href="{{path('app_home')}}" class="logo-link">
                         <img
                             src={logoDarkGreen}
                             alt="logo GM_Web"
-                            class="logo-icon"
+                            className="logo-icon"
                         />
                     </a>
                 </div>
+            </div>
+            <div className="comment">
+                <p>Développeur</p>
+                <p>Full-Stack</p>
             </div>
         </header>
     );
