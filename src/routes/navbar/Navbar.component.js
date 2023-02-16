31535631f8c13ba6a6222f545290ba9b28466d77
Grégory Marini <greg.marini@hotmail.fr>
2023-02-16 18:52:58 +0100

    adapt style in nav-bar

--- src/routes/navbar/Navbar.component.js
+++ src/routes/navbar/Navbar.component.js
@@ -1,12 +1,21 @@
 import { Routes, Route } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 import Home from "../home/Home.component";
 import "./navbar.styles.scss";
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleBtnclick = () => {
+        setIsOpen((prevIsOpen) => !prevIsOpen);
+    };
+
     return (
-        <section className="navbar-container">
-            <button className="navbar-container__button">
+        <section className={`navbar-container ${isOpen ? "nav-open" : ""}`}>
+            <button
+                className="navbar-container__button"
+                onClick={handleBtnclick}
+            >
                 <span className="navbar-container__icon">&nbsp;</span>
             </button>
             <div className="navbar">
