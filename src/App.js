96b0e721ebbf234bfe90dcc9f2bbe0bb77ffd968
Grégory Marini <greg.marini@hotmail.fr>
2023-03-07 18:02:05 +0100

    deplace load project in app(improve performance)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import "./App.styles.scss";
-import { Routes, Route } from "react-router-dom";
-import Hero from "./components/header/Hero.component";
-import Navbar from "./routes/navbar/Navbar.component";
-import Portefolio from "./components/portefolio/Portefolio.component";
-import TrainReact from "./components/trainReact/TrainReact.component";
-import Todos from "./components/trainReact/todosWithMax/Todos/Todos.component";
-import MeetupsHome from "./components/trainReact/meetupsWithMax/MeetupsHome";
+import './App.styles.scss';
+import { Routes, Route } from 'react-router-dom';
+import Hero from './components/header/Hero.component';
+import Navbar from './routes/navbar/Navbar.component';
+import Portefolio from './components/portefolio/Portefolio.component';
+import TrainReact from './components/trainReact/TrainReact.component';
+import Todos from './components/trainReact/todosWithMax/Todos/Todos.component';
+import MeetupsHome from './components/trainReact/meetupsWithMax/MeetupsHome';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { setProjects } from './store/projects/project.action';
+import { projectsdata } from './utils/data/projects.data';
 
 const App = () => {
-    return (
-        <div className="app-container">
-            <Routes>
-                <Route path="/" element={<Navbar />}>
-                    <Route index element={<Hero />} />
-                    <Route path="portefolio" element={<Portefolio />} />
-                    <Route path="trainReact/*" element={<TrainReact />} />
-                    <Route path="trainReact/todos" element={<Todos />} />
-                    <Route
-                        path="trainReact/meetupsHome/*"
-                        element={<MeetupsHome />}
-                    />
-                </Route>
-            </Routes>
-        </div>
-    );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setProjects(projectsdata));
+  }, [dispatch]);
+
+  return (
+    <div className='app-container'>
+      <Routes>
+        <Route path='/' element={<Navbar />}>
+          <Route index element={<Hero />} />
+          <Route path='portefolio' element={<Portefolio />} />
+          <Route path='trainReact/*' element={<TrainReact />} />
+          <Route path='trainReact/todos' element={<Todos />} />
+          <Route path='trainReact/meetupsHome/*' element={<MeetupsHome />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- src/components/portefolio/Portefolio.component.js
+++ src/components/portefolio/Portefolio.component.js
@@ -1,32 +1,24 @@
-import { Fragment, useEffect } from "react";
-import Project from "../project/Project.component";
-import "./portefolio.styles.scss";
-import { setProjects } from "../../store/projects/project.action";
-import { useDispatch, useSelector } from "react-redux";
-import { projectsdata } from "../../utils/data/projects.data";
-import { selectProjects } from "../../store/projects/project.selector";
+import { Fragment } from 'react';
+import Project from '../project/Project.component';
+import './portefolio.styles.scss';
+import { selectProjects } from '../../store/projects/project.selector';
+import { useSelector } from 'react-redux';
 
 const Portefolio = () => {
-    const dispatch = useDispatch();
+  const projects = useSelector(selectProjects);
+  // console.log(projects);
 
-    useEffect(() => {
-        dispatch(setProjects(projectsdata));
-    }, [dispatch]);
-
-    const projects = useSelector(selectProjects);
-    // console.log(projects);
-
-    return (
-        <Fragment>
-            <div className="portfolio-container full-container">
-                <h2 className="portfolio-title">Portfolio</h2>
-                {projects &&
-                    projects.map((project) => (
-                        <Project key={project.id} project={project} />
-                    ))}
-            </div>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <div className='portfolio-container full-container'>
+        <h2 className='portfolio-title'>Portfolio</h2>
+        {projects &&
+          projects.map((project) => (
+            <Project key={project.id} project={project} />
+          ))}
+      </div>
+    </Fragment>
+  );
 };
 
 export default Portefolio;
