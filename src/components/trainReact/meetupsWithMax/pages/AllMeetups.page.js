c263ae68e4035eb0191007b00f2440d24150e025
Grégory Marini <greg.marini@hotmail.fr>
2023-03-07 17:41:56 +0100

    add fetch in allMeetupsPage

--- src/components/trainReact/meetupsWithMax/pages/AllMeetups.page.js
+++ src/components/trainReact/meetupsWithMax/pages/AllMeetups.page.js
@@ -1,30 +1,55 @@
+import { useEffect, useState } from 'react';
 import MeetupList from '../meetups/MeetupList.meetups';
 import styles from './AllMeetups.module.scss';
 
 const DUMMY_DATA = [
-    {
-        id: 'm1',
-        title: 'This is a first meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-    {
-        id: 'm2',
-        title: 'This is a second meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
+  {
+    id: 'm1',
+    title: 'This is a first meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+  {
+    id: 'm2',
+    title: 'This is a second meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
 ];
 
 export default function AllMeetupsPage() {
-    return (
-        <section className={styles.allMeetups}>
-            <h1 className='meetup-h1'>All Meetups</h1>
-            <MeetupList meetups={DUMMY_DATA} />
-        </section>
-    );
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    const datas = async () => {
+      await fetch(
+        'www.https://gmweb-react-default-rtdb.europe-west1.firebasedatabase.app/meetups'
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          return data;
+        });
+    };
+    setIsLoading(false);
+    setLoadedMeetups(datas);
+  }, []);
+
+  console.log(loadedMeetups);
+
+  return (
+    <section className={styles.allMeetups}>
+      <h1 className='meetup-h1'>All Meetups</h1>
+      {isLoading ? <p>Loading page...</p> : <MeetupList meetups={DUMMY_DATA} />}
+    </section>
+  );
 }
--- src/components/trainReact/meetupsWithMax/pages/NewMeetup.page.js
+++ src/components/trainReact/meetupsWithMax/pages/NewMeetup.page.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import NewMeetupForm from '../meetups/NewMeetupForm.meetups';
 
 export default function NewMeetupPage() {
+    let navigate = useNavigate();
     const addMeetupHandler = (meetupData) => {
         fetch(
-            'https://gmweb-react-default-rtdb.europe-west1.firebasedatabase.app/meetups',
+            'www.https://gmweb-react-default-rtdb.europe-west1.firebasedatabase.app/meetups',
             {
                 method: 'POST',
                 body: JSON.stringify(meetupData),
                 headers: {
-                    'access-control-allow-origin': '*',
                     'Content-Type': 'application/json',
                 },
             }
-        );
+        ).then(() => {
+            navigate('/trainReact/meetupsHome');
+        });
     };
 
     return (
