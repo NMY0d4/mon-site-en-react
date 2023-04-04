ce108193a3ded66f835eaf40ede7405bba02aa37
Grégory <greg.marini@hotmail.fr>
2023-04-04 15:21:25 +0200

    work on style globaly(train project)

--- src/components/trainReact/TrainReact.component.js
+++ src/components/trainReact/TrainReact.component.js
@@ -1,22 +1,28 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-import "./trainReact.styles.scss";
+import './trainReact.styles.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faList, faLocation } from '@fortawesome/free-solid-svg-icons';
 
 const TrainReact = () => {
-    return (
-        <div className="train-container">
-            <h1>Trainning day</h1>
-            <ul>
-                <li>
-                    <NavLink to="todos">Todos-list</NavLink>
-                </li>
-                <li>
-                    <NavLink to="meetupsHome">Meetups Project</NavLink>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className='train-container'>
+      <h1>Trainning day</h1>
+      <ul>
+        <li>
+          <NavLink to='todos'>
+            <FontAwesomeIcon className='link-trainReact' icon={faList} />
+            Todos-list
+          </NavLink>
+        </li>
+        <li>
+          <FontAwesomeIcon className='link-trainReact' icon={faLocation} />
+          <NavLink to='meetupsHome'>Meetups Project</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default TrainReact;
