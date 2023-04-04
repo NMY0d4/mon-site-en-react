f52c60e9e8809de787c69ccbd463875536f20960
Grégory <greg.marini@hotmail.fr>
2023-04-04 16:26:18 +0200

    close computer

--- src/components/trainReact/todosWithMax/Todos/Todos.component.js
+++ src/components/trainReact/todosWithMax/Todos/Todos.component.js
@@ -1,15 +1,15 @@
-import Todo from "../Todo/Todo.component";
-import "./todos.styles.scss";
+import Todo from '../Todo/Todo.component';
+import './todos.styles.scss';
 
 export default function Todos() {
-    return (
-        <div className="full-container">
-            <h1 className="title-train">My Todos</h1>
-            <div className="todos-container center-container">
-                <Todo text="Learn React" />
-                <Todo text="Master React" />
-                <Todo text="Explore the full React Course" />
-            </div>
-        </div>
-    );
+  return (
+    <div className='full-container'>
+      <h1 className='title-train text-center'>My Todos</h1>
+      <div className='todos-container center-container'>
+        <Todo text='Learn React' />
+        <Todo text='Master React' />
+        <Todo text='Explore the full React Course' />
+      </div>
+    </div>
+  );
 }
